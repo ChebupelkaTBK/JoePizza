058f04ebf68828752cc288e295ed6536d78d8c16
ChebupelkaTBK <mikhailbelogrivtsev2007@gmail.com>
2025-11-22 16:28:14 +0300

    not data

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,15 +4,12 @@ import SectionBestDeals from "./section/SectionBestDeals/SectionBestDeals.jsx";
 import SectionCombo from "./section/SectionCombo/SectionCombo.jsx";
 
 export default function Main() {
-  const [now, setNow] = useState(new Date());
-  setInterval(() => setNow(new Date()), 1000);
   return (
     <main>
       <h1>Лучшие предложения!</h1>
       <SectionBestDeals />
       <h1>Комбо</h1>
       <SectionCombo />
-      <span>{Date()}</span>
     </main>
   );
 }
